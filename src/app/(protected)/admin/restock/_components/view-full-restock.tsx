1c8df83a70f65b12a21223d86abe7c068fb38401
Joshua Sevilla <jSevilla@mcm.edu.ph>
2024-12-30 07:43:39 +0800

    fix error

--- src/app/(protected)/admin/restock/_components/view-full-restock.tsx
+++ src/app/(protected)/admin/restock/_components/view-full-restock.tsx
@@ -14,6 +14,8 @@ import {
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import { Separator } from "~/components/ui/separator";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "~/components/ui/table";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "~/components/ui/hover-card";
 import RecordEditor from "../../_components/record-editor";
 
 const poppins = Poppins({
@@ -26,14 +28,28 @@ type RestockProps = {
   date: string;
   supplier: string;
   addedStock: number;
+  restockItems: {
+    variant: string;
+    item: string;
+    brand: string;
+    quantity: number;
+    mainUnit: string;
+    unitConversion: {
+      from: string;
+      count: number;
+      to: string;
+    }[];
+  }[];
 };
 
 const ViewFullRestock: React.FC<RestockProps> = ({
-  restockId,
-  date,
-  supplier,
-  addedStock,
-}) => {
+                                                   restockId,
+                                                   date,
+                                                   supplier,
+                                                   addedStock,
+                                                   restockItems = [], // Default to an empty array
+                                                 }) => {
+
   const [isEditing, setIsEditing] = useState(false);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [showWarning, setShowWarning] = useState(false);
@@ -44,108 +60,135 @@ const ViewFullRestock: React.FC<RestockProps> = ({
   };
 
   const handleKeyDown = (event: React.KeyboardEvent) => {
-    console.log("Key pressed:", event.key); // Log the key pressed
-    if (event.key === "Escape") {
-      console.log("Escape key pressed");
-      if (isEditing) {
-        console.log("Editing is enabled, showing warning");
-        setShowWarning(true); // Ensure state is updated
-        event.preventDefault();
-      }
+    if (event.key === "Escape" && isEditing) {
+      setShowWarning(true);
+      event.preventDefault();
     }
   };
 
   return (
-    <Dialog
-      open={isDialogOpen}
-      onOpenChange={(open) => {
-        if (!isEditing) {
-          setIsDialogOpen(open);
-        }
-      }}
-    >
-      <DialogTrigger>
-        <div className="flex items-center gap-2 rounded-lg px-3 py-2 tracking-wide text-slate-400 transition-colors duration-300 hover:bg-slate-200 hover:text-slate-500">
-          View All
-          <ArrowUpRight strokeWidth={2.5} className="h-4 w-4" />
-        </div>
-      </DialogTrigger>
-      <DialogContent
-        className="!w-full !max-w-3xl [&>button]:hidden"
-        onKeyDown={handleKeyDown}
+      <Dialog
+          open={isDialogOpen}
+          onOpenChange={(open) => {
+            if (!isEditing) setIsDialogOpen(open);
+          }}
       >
-        <DialogHeader
-          className={`text-xl ${poppins.className} h-full font-normal`}
+        <DialogTrigger>
+          <div className="flex items-center gap-2 rounded-lg px-3 py-2 tracking-wide text-slate-400 transition-colors duration-300 hover:bg-slate-200 hover:text-slate-500">
+            View All
+            <ArrowUpRight strokeWidth={2.5} className="h-4 w-4" />
+          </div>
+        </DialogTrigger>
+        <DialogContent
+            className="!w-full !max-w-3xl [&>button]:hidden"
+            onKeyDown={handleKeyDown}
         >
-          <div className="flex items-center justify-between">
-            <div className="flex h-full flex-col justify-between gap-2">
-              <DialogTitle className="text-xl font-normal text-slate-700">
-                #{restockId}
-              </DialogTitle>
-              <div className="flex items-center gap-3 text-slate-400">
-                <Calendar className="h-4 w-4" />
-                <DialogDescription className="text-sm tracking-wide">
-                  {date}
-                </DialogDescription>
+          <DialogHeader className={`text-xl ${poppins.className} font-normal`}>
+            <div className="flex items-center justify-between">
+              <div className="flex flex-col gap-2">
+                <DialogTitle className="text-xl font-normal text-slate-700">
+                  #{restockId}
+                </DialogTitle>
+                <div className="flex items-center gap-3 text-slate-400">
+                  <Calendar className="h-4 w-4" />
+                  <DialogDescription className="text-sm tracking-wide">
+                    {date}
+                  </DialogDescription>
+                </div>
               </div>
+              <RecordEditor isEditing={isEditing} handleEdit={handleEdit} />
             </div>
-            <RecordEditor isEditing={isEditing} handleEdit={handleEdit} />
-          </div>
-        </DialogHeader>
-        <Separator orientation="horizontal" className="h-[2px]" />
-        <div className="flex gap-3">
-          <div className="group flex w-1/2 flex-col gap-2">
-            <Label className="text-slate-400">Supplier</Label>
-            <div className="flex items-center rounded-lg focus-within:outline focus-within:outline-2 focus-within:outline-slate-200">
+          </DialogHeader>
+          <Separator orientation="horizontal" className="h-[2px]" />
+          <div className="flex gap-3">
+            <div className="flex w-1/2 flex-col gap-2">
+              <Label className="text-slate-400">Supplier</Label>
               <Input
-                className="bg-slate-100 text-slate-700 shadow-none"
-                disabled={!isEditing}
-                value={supplier}
+                  className="bg-slate-100 text-slate-700 shadow-none"
+                  disabled={!isEditing}
+                  value={supplier}
+              />
+            </div>
+            <div className="flex w-1/2 flex-col gap-2">
+              <Label className="text-slate-400">Recorded by</Label>
+              <Input
+                  className="bg-slate-100 text-slate-700 shadow-none"
+                  disabled={!isEditing}
               />
             </div>
           </div>
-          <div className="group flex w-1/2 flex-col gap-2">
-            <Label className="text-slate-400">Recorded by</Label>
-            <div className="flex items-center rounded-lg focus-within:outline focus-within:outline-2 focus-within:outline-slate-200">
-              <Input
-                className="bg-slate-100 text-slate-700 shadow-none"
-                disabled={!isEditing}
-              />
-            </div>
+          <div className="mt-4 overflow-x-auto">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Item</TableHead>
+                  <TableHead>Quantity</TableHead>
+                  <TableHead>Unit</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {restockItems.length > 0 ? (
+                    restockItems.map((item, index) => (
+                        <TableRow key={index}>
+                          <TableCell>
+                            {item.item} - {item.brand} - {item.variant}
+                          </TableCell>
+                          <TableCell>{item.quantity}</TableCell>
+                          <TableCell>
+                            <div className="flex items-center gap-2">
+                              <HoverCard>
+                                <HoverCardTrigger className="hover:underline text-sm text-textGray">
+                                  {item.mainUnit} ({item.unitConversion.length} Conversions)
+                                </HoverCardTrigger>
+                                <HoverCardContent className="shadow-none flex flex-col gap-3">
+                                  {item.unitConversion.length > 0 ? (
+                                      item.unitConversion.map((conversion, index) => (
+                                          <div key={index}>
+                                            {conversion.from} → {conversion.count} {conversion.to}
+                                          </div>
+                                      ))
+                                  ) : (
+                                      <div className="text-xs text-slate-400">No conversions available</div>
+                                  )}
+                                </HoverCardContent>
+                              </HoverCard>
+                            </div>
+                          </TableCell>
+                        </TableRow>
+                    ))
+                ) : (
+                    <TableRow>
+                      <TableCell colSpan={3}>No items available</TableCell>
+                    </TableRow>
+                )}
+              </TableBody>
+            </Table>
           </div>
-        </div>
-        <div className="">~ A table should exist here ~</div>
-        <Separator orientation="horizontal" className="h-[2px]" />
-        <div className="flex items-center justify-between">
-          <div className="flex h-full flex-col justify-between gap-1">
-            <p className="text-base font-normal text-slate-700">{addedStock}</p>
-            <div className="flex items-center gap-3 text-slate-400">
-              <p className="text-sm tracking-wide">Added Stock</p>
+          <Separator orientation="horizontal" className="h-[2px]" />
+          <div className="flex items-center justify-between">
+            <div className="flex flex-col">
+              <p className="text-base font-normal text-slate-700">{addedStock}</p>
+              <p className="text-sm text-slate-400">Added Stock</p>
             </div>
-          </div>
-          <div className="flex items-center gap-2">
-            <DialogClose asChild disabled={isEditing}>
-              <Button
-                variant={"secondary"}
-                className="text-slate-700 hover:bg-slate-200"
-                disabled={isEditing}
-              >
-                Close
+            <div className="flex items-center gap-2">
+              <DialogClose asChild disabled={isEditing}>
+                <Button variant="secondary" className="text-slate-700 hover:bg-slate-200" disabled={isEditing}>
+                  Close
+                </Button>
+              </DialogClose>
+              <Button className="bg-green hover:bg-green/80" disabled={isEditing}>
+                <Printer />
+                Print Invoice
               </Button>
-            </DialogClose>
-            <Button className="bg-green hover:bg-green/80" disabled={isEditing}>
-              <Printer />
-              Print Invoice
-            </Button>
+            </div>
           </div>
-        </div>
-        {showWarning && (
-          <p className="text-right text-sm text-orange-400">
-            Whoops! Don't forget to save your changes.
-          </p>
-        )}
-      </DialogContent>
-    </Dialog>
+          {showWarning && (
+              <p className="text-right text-sm text-orange-400">
+                Whoops! Don't forget to save your changes.
+              </p>
+          )}
+        </DialogContent>
+      </Dialog>
   );
 };
 

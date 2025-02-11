04f297d1c5bc51a66e1da985a757958c0080989a
Lolillya <SomeonesUsernam@gmail.com>
2025-02-11 17:11:11 +0800

    REMOVE: void button

--- src/app/(protected)/admin/invoice/_components/invoice-record.tsx
+++ src/app/(protected)/admin/invoice/_components/invoice-record.tsx
@@ -22,113 +22,117 @@ const InvoiceRecord: React.FC<InvoiceProps> = ({
   line_items,
 }) => {
   return (
-    console.log(invoiceId,
+    console.log(
+      invoiceId,
       date,
       customer,
       invoiceClerk,
       grandTotal,
-      line_items),
-    <div className="flex flex-col gap-8 rounded-lg bg-slate-100 p-10 text-slate-700">
-      <div className="flex items-center justify-center px-6">
-        <div className="flex w-1/2 flex-col gap-4">
-          <p className="text-xl">#{invoiceId}</p>
-          <div className="flex items-center gap-6 text-slate-400">
-            <div className="flex items-center gap-3 text-slate-400">
-              <Calendar className="h-4 w-4" />
-              <p className="text-sm">
-                {date.toLocaleDateString("en-US", {
-                  month: "long",
-                  day: "numeric",
-                  year: "numeric",
-                })}
-              </p>
-            </div>
-            <Separator
-              orientation="vertical"
-              className="h-6 w-[2px] bg-slate-200"
-            />
-            <div className="flex items-center gap-3">
-              <Users2 className="h-4 w-4" />
-              <p className="text-sm">{customer}</p>
-            </div>
-          </div>
-        </div>
-
-        <Separator
-          orientation="vertical"
-          className="h-16 w-[2px] rounded-lg bg-slate-200"
-        />
-
-        <div className="flex w-1/2 items-center justify-between pl-8">
-          <div className="flex flex-col gap-4">
-            <p className="text-xl">₱ {grandTotal.toLocaleString()}</p>
-            <div className="flex items-center gap-8 text-slate-400">
+      line_items,
+    ),
+    (
+      <div className="flex flex-col gap-8 rounded-lg bg-slate-100 p-10 text-slate-700">
+        <div className="flex items-center justify-center px-6">
+          <div className="flex w-1/2 flex-col gap-4">
+            <p className="text-xl">#{invoiceId}</p>
+            <div className="flex items-center gap-6 text-slate-400">
               <div className="flex items-center gap-3 text-slate-400">
-                <p className="text-sm">Grand Total</p>
+                <Calendar className="h-4 w-4" />
+                <p className="text-sm">
+                  {date.toLocaleDateString("en-US", {
+                    month: "long",
+                    day: "numeric",
+                    year: "numeric",
+                  })}
+                </p>
+              </div>
+              <Separator
+                orientation="vertical"
+                className="h-6 w-[2px] bg-slate-200"
+              />
+              <div className="flex items-center gap-3">
+                <Users2 className="h-4 w-4" />
+                <p className="text-sm">{customer}</p>
               </div>
             </div>
           </div>
-          <div>
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                <MoreOptions />
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="text-slate-700 shadow-none">
-                <DropdownMenuItem className="hover:!bg-slate-200 focus:!bg-slate-200">
-                  Print
-                </DropdownMenuItem>
-                <DropdownMenuItem className="hover:!bg-slate-200 focus:!bg-slate-200">
-                  Export
-                </DropdownMenuItem>
-                <DropdownMenuItem className="hover:!bg-slate-200 focus:!bg-slate-200">
-                  Pin
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem className="hover:!bg-slate-200 focus:!bg-slate-200">
-                  View Invoice
-                </DropdownMenuItem>
-                <DropdownMenuItem className="hover:!bg-slate-200 focus:!bg-slate-200">
-                  View Customer
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem className="text-red hover:!bg-rose-200 hover:!text-red focus:!bg-rose-200 focus:!text-red">
+
+          <Separator
+            orientation="vertical"
+            className="h-16 w-[2px] rounded-lg bg-slate-200"
+          />
+
+          <div className="flex w-1/2 items-center justify-between pl-8">
+            <div className="flex flex-col gap-4">
+              <p className="text-xl">₱ {grandTotal.toLocaleString()}</p>
+              <div className="flex items-center gap-8 text-slate-400">
+                <div className="flex items-center gap-3 text-slate-400">
+                  <p className="text-sm">Grand Total</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <MoreOptions />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="text-slate-700 shadow-none">
+                  <DropdownMenuItem className="hover:!bg-slate-200 focus:!bg-slate-200">
+                    Print
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="hover:!bg-slate-200 focus:!bg-slate-200">
+                    Export
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="hover:!bg-slate-200 focus:!bg-slate-200">
+                    Pin
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem className="hover:!bg-slate-200 focus:!bg-slate-200">
+                    View Invoice
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="hover:!bg-slate-200 focus:!bg-slate-200">
+                    View Customer
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  {/* <DropdownMenuItem className="text-red hover:!bg-rose-200 hover:!text-red focus:!bg-rose-200 focus:!text-red">
                   Void
-                </DropdownMenuItem>
-                <DropdownMenuItem className="text-red hover:!bg-rose-200 hover:!text-red focus:!bg-rose-200 focus:!text-red">
-                  Delete
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+                </DropdownMenuItem> */}
+                  <DropdownMenuItem className="text-red hover:!bg-rose-200 hover:!text-red focus:!bg-rose-200 focus:!text-red">
+                    Delete
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-3">
+          {line_items.slice(0, 2).map((item, index) => {
+            return <InvoiceItem key={index} line_items={item} />;
+            // console.log(item);
+          })}
+
+          <div className="flex items-center justify-between rounded-lg bg-white/70 px-6 py-3 text-slate-400">
+            {line_items.length > 2 ? (
+              <p>
+                {line_items.length - 2} more item
+                {line_items.length - 2 > 1 ? "s" : ""}...
+              </p>
+            ) : (
+              line_items.length <= 2 && <p>No more items...</p>
+            )}
+            <ViewFullInvoice
+              invoiceId={invoiceId}
+              date={date}
+              customer={customer}
+              invoiceClerk={invoiceClerk}
+              grandTotal={grandTotal}
+              line_items={line_items}
+            />
           </div>
         </div>
       </div>
-
-      <div className="flex flex-col gap-3">
-        {line_items.slice(0, 2).map((item, index) => {
-          return <InvoiceItem key={index} line_items={item} />;
-          // console.log(item);
-        })}
-
-        <div className="flex items-center justify-between rounded-lg bg-white/70 px-6 py-3 text-slate-400">
-          {line_items.length > 2 ? (
-            <p>
-              {line_items.length - 2} more item
-              {line_items.length - 2 > 1 ? "s" : ""}...
-            </p>
-          ) : (
-            line_items.length <= 2 && <p>No more items...</p>
-          )}
-          <ViewFullInvoice
-            invoiceId={invoiceId}
-            date={date}
-            customer={customer}
-            invoiceClerk={invoiceClerk}
-            grandTotal={grandTotal}
-            line_items={line_items}
-          />
-        </div>
-      </div>
-    </div>
+    )
   );
 };
 

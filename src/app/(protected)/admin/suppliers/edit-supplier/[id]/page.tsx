ab267ddbd04565b6cee72e555dfdea53ef10f1c7
Lolillya <someonesusernam@gmail.com>
2025-03-11 03:12:09 +0800

    FIX: edit-supplier params

--- src/app/(protected)/admin/suppliers/edit-supplier/[id]/page.tsx
+++ src/app/(protected)/admin/suppliers/edit-supplier/[id]/page.tsx
@@ -1,13 +1,15 @@
-// "use client";
+"use client";
+import { useParams } from "next/navigation";
 import EditSupplierState from "../../_components/edit-supplier";
 import SupplierRouter from "../../_components/supplier-router";
 
-const EditSupplier = ({ params }: { params: { id: string } }) => {
-  const id = params.id;
+const EditSupplier = () => {
+  const { id } = useParams();
+  const idString = String(id);
   return (
     <section className="flex h-screen w-screen flex-col gap-3 p-10">
       <SupplierRouter title="EDIT SUPPLIER" />
-      <EditSupplierState id={id} />
+      <EditSupplierState id={idString} />
     </section>
   );
 };

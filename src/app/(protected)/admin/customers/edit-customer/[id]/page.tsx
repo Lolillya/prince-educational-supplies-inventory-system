7e37c8142d5907a136279cb6979fa72423ef6f58
Lolillya <someonesusernam@gmail.com>
2025-03-11 02:31:39 +0800

    TEMP FIX: edit-customer

--- src/app/(protected)/admin/customers/edit-customer/[id]/page.tsx
+++ src/app/(protected)/admin/customers/edit-customer/[id]/page.tsx
@@ -5,12 +5,12 @@ import CustomerRouter from "../../_components/customer-router";
 import EditCustomerState from "../../_components/edit-customer";
 
 const EditCustomer = () => {
-  const { id } = useParams();
-  const idString = String(id);
+  const id = "";
+  // const idString = String(id);
   return (
     <section className="flex h-screen w-screen flex-col gap-3 p-10">
       <CustomerRouter title="EDIT CUSTOMER" />
-      <EditCustomerState id={idString} />
+      <EditCustomerState id={id} />
     </section>
   );
 };

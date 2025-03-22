5a94a69c14afe946be6af6a2259102420ecd00d0
Joshua Sevilla <jSevilla@mcm.edu.ph>
2025-03-23 06:35:47 +0800

    newItem: variant minor input update

--- src/app/(protected)/admin/inventory/_components/variant.tsx
+++ src/app/(protected)/admin/inventory/_components/variant.tsx
@@ -1,67 +1,89 @@
-import React from 'react'
-import { Button } from '~/components/ui/button'
-import { Input } from '~/components/ui/input'
-import { X } from 'lucide-react'
+import React from "react";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
 
 interface VariantProps {
-	onRemove: () => void;
-	isOnlyVariant: boolean;
-	value?: {
-		variant: string;
-		lowStock: number;
-		veryLowStock: number;
-	};
-	onChange?: (value: {
-		variant: string;
-		lowStock: number;
-		veryLowStock: number;
-	}) => void;
+  onRemove: () => void;
+  isOnlyVariant: boolean;
+  value?: {
+    variant: string;
+    lowStock: number;
+    veryLowStock: number;
+  };
+  onChange?: (value: {
+    variant: string;
+    lowStock: number;
+    veryLowStock: number;
+  }) => void;
 }
 
-const Variant = ({ onRemove, isOnlyVariant, value, onChange }: VariantProps) => {
-	return (
-		<div className="bg-slate-100 p-4 rounded-lg flex flex-col gap-4">
-			<Input
-				placeholder="Variant Name"
-				className="bg-white text-slate-700 placeholder-slate-300 shadow-none"
-				value={value?.variant}
-				onChange={(e) => onChange?.({ ...value, variant: e.target.value })}
-			/>
-			<div className="flex gap-4">
-				<div className="flex items-center w-full">
-					<div className="w-2 h-full bg-amber-500 rounded-l-lg" />
-					<Input
-						type="number"
-						min="0"
-						placeholder="Low Stock"
-						className="bg-white text-slate-700 placeholder-slate-300 shadow-none w-full rounded-l-none"
-						value={value?.lowStock}
-						onChange={(e) => onChange?.({ ...value, lowStock: Number(e.target.value) })}
-					/>
-				</div>
-				<div className="flex items-center w-full">
-					<div className="w-2 h-full bg-rose-500 rounded-l-lg" />
-					<Input
-						type="number"
-						min="0"
-						placeholder="Very Low Stock"
-						className="bg-white text-slate-700 placeholder-slate-300 shadow-none w-full rounded-l-none"
-						value={value?.veryLowStock}
-						onChange={(e) => onChange?.({ ...value, veryLowStock: Number(e.target.value) })}
-					/>
-				</div>
-			</div>
-			{onRemove && (
-				<Button
-					className="bg-slate-100 rounded-lg p-2 text-slate-400 hover:bg-rose-100 border-[3px] border-dashed border-slate-300 hover:border-rose-300 hover:text-rose-400 mt-2"
-					onClick={onRemove}
-					disabled={isOnlyVariant}
-				>
-					Remove Variant
-				</Button>
-			)}
-		</div>
-	)
-}
+const Variant = ({
+  onRemove,
+  isOnlyVariant,
+  value,
+  onChange,
+}: VariantProps) => {
+  return (
+    <div className="flex flex-col gap-4 rounded-lg bg-slate-100 p-4">
+      <Input
+        placeholder="Variant Name"
+        className="bg-white text-slate-700 placeholder-slate-300 shadow-none"
+        value={value?.variant}
+        onChange={(e) => {
+          const filteredValue = e.target.value.replace(/[^a-zA-Z0-9 ]/g, "");
+          onChange?.({ ...value, variant: filteredValue });
+        }}
+      />
+      <div className="flex gap-4">
+        <div className="flex w-full items-center">
+          <div className="h-full w-2 rounded-l-lg bg-amber-500" />
+          <Input
+            type="number"
+            min="0"
+            placeholder="Low Stock"
+            className="w-full rounded-l-none bg-white text-slate-700 placeholder-slate-300 shadow-none"
+            value={
+              value?.lowStock === 0 || value?.lowStock === undefined
+                ? ""
+                : value?.lowStock
+            }
+            onChange={(e) =>
+              onChange?.({ ...value, lowStock: Number(e.target.value) || 0 })
+            }
+          />
+        </div>
+        <div className="flex w-full items-center">
+          <div className="h-full w-2 rounded-l-lg bg-rose-500" />
+          <Input
+            type="number"
+            min="0"
+            placeholder="Very Low Stock"
+            className="w-full rounded-l-none bg-white text-slate-700 placeholder-slate-300 shadow-none"
+            value={
+              value?.veryLowStock === 0 || value?.veryLowStock === undefined
+                ? ""
+                : value?.veryLowStock
+            }
+            onChange={(e) =>
+              onChange?.({
+                ...value,
+                veryLowStock: Number(e.target.value) || 0,
+              })
+            }
+          />
+        </div>
+      </div>
+      {onRemove && (
+        <Button
+          className="mt-2 rounded-lg border-[3px] border-dashed border-slate-300 bg-slate-100 p-2 text-slate-400 hover:border-rose-300 hover:bg-rose-100 hover:text-rose-400"
+          onClick={onRemove}
+          disabled={isOnlyVariant}
+        >
+          Remove Variant
+        </Button>
+      )}
+    </div>
+  );
+};
 
-export default Variant
+export default Variant;

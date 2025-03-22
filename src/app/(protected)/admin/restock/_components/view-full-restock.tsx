8ac63798c1ec11e02c2eb252bd306999301db2df
KenjiM-03 <kenjimende7@gmail.com>
2025-03-22 22:30:59 +0800

    ADD: If there is input

--- src/app/(protected)/admin/restock/_components/view-full-restock.tsx
+++ src/app/(protected)/admin/restock/_components/view-full-restock.tsx
@@ -16,7 +16,6 @@ import { Label } from "~/components/ui/label";
 import { Separator } from "~/components/ui/separator";
 import { Textarea } from "~/components/ui/textarea";
 import RecordEditor from "../../_components/record-editor";
-import RecordExpand from "../../_components/record-expand";
 import { type RestockProps } from "../page";
 import RestockTable from "./restock-table";
 
@@ -26,17 +25,17 @@ const poppins = Poppins({
 });
 
 const ViewFullRestock: React.FC<RestockProps> = ({
-  restockId,
-  date,
-  supplier,
-  restockClerk,
-  addedStock,
-  restockItems = [], // Default to an empty array
-}) => {
-
+                                                   restockId,
+                                                   date,
+                                                   supplier,
+                                                   restockClerk,
+                                                   addedStock,
+                                                   restockItems = [],
+                                                 }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [showWarning, setShowWarning] = useState(false);
+  const [hasChanges, setHasChanges] = useState<boolean>(false);
 
   const handleEdit = () => {
     setIsEditing((prev) => !prev);
@@ -50,107 +49,85 @@ const ViewFullRestock: React.FC<RestockProps> = ({
     }
   };
 
+  const handleTextareaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setHasChanges(event.target.value.trim() !== "");
+  };
+
   return (
-    <Dialog
-      open={isDialogOpen}
-      onOpenChange={(open) => {
-        if (!open) {
-          if (isEditing) {
-            setShowWarning(true);
-            return;
-          }
-        }
-        setIsDialogOpen(open);
-        if (!open) {
-          setShowWarning(false);
-        }
-      }}
-    >
-      <DialogTrigger>
-        <div className="flex items-center gap-2 rounded-lg px-3 py-2 tracking-wide text-slate-400 transition-colors duration-300 hover:bg-slate-200 hover:text-slate-500">
-          View Details
-          <ArrowUpRight strokeWidth={2.5} className="h-4 w-4" />
-        </div>
-      </DialogTrigger>
-      <DialogContent
-        className="!w-full !max-w-3xl [&>button]:hidden"
-        onKeyDown={handleKeyDown}
+      <Dialog
+          open={isDialogOpen}
+          onOpenChange={(open) => {
+            if (!open && isEditing) {
+              setShowWarning(true);
+              return;
+            }
+            setIsDialogOpen(open);
+            setShowWarning(false);
+          }}
       >
-        <DialogHeader className={`text-xl ${poppins.className} font-normal`}>
-          <div className="flex items-center justify-between">
-            <div className="flex flex-col gap-2">
-              <DialogTitle className="text-xl font-normal text-slate-700">
-                #{restockId}
-              </DialogTitle>
-              <div className="flex items-center gap-3 text-slate-400">
-                <Calendar className="h-4 w-4" />
-                <DialogDescription className="text-sm tracking-wide">
-                  {date}
-                </DialogDescription>
+        <DialogTrigger>
+          <div className="flex items-center gap-2 rounded-lg px-3 py-2 tracking-wide text-slate-400 transition-colors duration-300 hover:bg-slate-200 hover:text-slate-500">
+            View Details
+            <ArrowUpRight strokeWidth={2.5} className="h-4 w-4" />
+          </div>
+        </DialogTrigger>
+        <DialogContent className="!w-full !max-w-3xl [&>button]:hidden" onKeyDown={handleKeyDown}>
+          <DialogHeader className={`text-xl ${poppins.className} font-normal`}>
+            <div className="flex items-center justify-between">
+              <div className="flex flex-col gap-2">
+                <DialogTitle className="text-xl font-normal text-slate-700">#{restockId}</DialogTitle>
+                <div className="flex items-center gap-3 text-slate-400">
+                  <Calendar className="h-4 w-4" />
+                  <DialogDescription className="text-sm tracking-wide">{date}</DialogDescription>
+                </div>
               </div>
-            </div>
-            <div className="flex items-center gap-3">
               <RecordEditor isEditing={isEditing} handleEdit={handleEdit} />
-              {/*<RecordExpand />*/}
+            </div>
+          </DialogHeader>
+
+          <Separator orientation="horizontal" className="h-[2px]" />
+
+          <div className="flex gap-3">
+            <div className="flex w-1/2 flex-col gap-2">
+              <Label className="text-slate-400">Supplier</Label>
+              <Input className="bg-slate-100 text-slate-700 shadow-none" disabled={!isEditing} value={supplier ?? ''} />
+            </div>
+            <div className="flex w-1/2 flex-col gap-2">
+              <Label className="text-slate-400">Recorded by</Label>
+              <Input className="bg-slate-100 text-slate-700 shadow-none" disabled={!isEditing} value={restockClerk} />
             </div>
           </div>
-        </DialogHeader>
 
-        <Separator orientation="horizontal" className="h-[2px]" />
+          <RestockTable restockItem={restockItems} isEditing={isEditing} />
 
-        <div className="flex gap-3">
-          <div className="flex w-1/2 flex-col gap-2">
-            <Label className="text-slate-400">Supplier</Label>
-            <Input
-              className="bg-slate-100 text-slate-700 shadow-none"
+          <Separator orientation="horizontal" className="h-[2px]" />
+
+          <Textarea
+              className="!min-h-16 border-none text-slate-700 bg-slate-100 resize-none focus:outline focus:outline-2 focus:outline-slate-200"
+              placeholder="Your record notes..."
               disabled={!isEditing}
-              value={supplier ?? ''}
-            />
-          </div>
-          <div className="flex w-1/2 flex-col gap-2">
-            <Label className="text-slate-400">Recorded by</Label>
-            <Input
-              className="bg-slate-100 text-slate-700 shadow-none"
-              disabled={!isEditing}
-              value={restockClerk}
-            />
-          </div>
-        </div>
+              onChange={handleTextareaChange}
+          />
 
-        <RestockTable restockItem={restockItems} isEditing={isEditing} />
-
-        <Separator orientation="horizontal" className="h-[2px]" />
-
-        <Textarea
-          className="!min-h-16 border-none text-slate-700 bg-slate-100 resize-none focus:outline focus:outline-2 focus:outline-slate-200"
-          placeholder="Your record notes..."
-          disabled={!isEditing}
-        />
-
-        <div className="flex items-center justify-between">
-          <div className="flex flex-col">
+          <div className="flex items-center justify-between">
             <p className="text-base font-normal text-slate-700">{addedStock}</p>
-            <p className="text-sm text-slate-400">Added Stock</p>
-          </div>
-          <div className="flex items-center gap-2">
-            <DialogClose asChild disabled={isEditing}>
-              <Button variant="secondary" className="text-slate-700 hover:bg-slate-200" disabled={isEditing}>
-                Close
+            <div className="flex items-center gap-2">
+              <Button
+                  variant={"secondary"}
+                  className="text-slate-700 hover:bg-slate-200"
+                  disabled
+              >
+                Save Changes
               </Button>
-            </DialogClose>
-            <Button className="bg-green hover:bg-green/80" disabled={isEditing}>
-              <Printer />
-              Print Restock
-            </Button>
+              <Button className="bg-green hover:bg-green/80" disabled={isEditing}>
+                <Printer />
+                Print Restock
+              </Button>
+            </div>
           </div>
-        </div>
-        {showWarning && (
-          <p className="text-right text-sm text-orange-400">
-            Whoops! Don't forget to save your changes.
-          </p>
-        )}
-      </DialogContent>
-    </Dialog>
+          {showWarning && <p className="text-right text-sm text-orange-400">Whoops! Don't forget to save your changes.</p>}
+        </DialogContent>
+      </Dialog>
   );
 };
 

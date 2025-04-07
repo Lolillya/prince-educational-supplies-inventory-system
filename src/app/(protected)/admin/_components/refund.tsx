5e23d8f76cabbd340181ac2cdb363e70b3c76a8a
Joshua Sevilla <jSevilla@mcm.edu.ph>
2025-04-07 21:35:08 +0800

    Customer: Payments

--- src/app/(protected)/admin/_components/refund.tsx
+++ src/app/(protected)/admin/_components/refund.tsx
@@ -1,53 +1,72 @@
 import { PopoverClose } from "@radix-ui/react-popover";
 import { Undo2 } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
-import { Popover, PopoverContent, PopoverTrigger } from "~/components/ui/popover";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/ui/popover";
 
 interface RefundProps {
-	className?: string;
+  className?: string;
+  onClick?: () => void;
 }
 
-const Refund: React.FC<RefundProps> = ({ className }) => {
-	return (
-		<Popover>
-			<PopoverTrigger>
-				<Button
-					variant="ghost"
-					className={`group flex h-12 w-12 items-center justify-center rounded-xl bg-transparent hover:bg-rose-200/60 transition-all duration-300 ${className}`}
-				>
-					<Undo2 className="!h-5 !w-5 text-slate-500 group-hover:text-rose-500 transition-colors duration-300" strokeWidth={2.5} />
-				</Button>
-			</PopoverTrigger>
-			<PopoverContent
-				className="shadow-none"
-				popoverTarget=""
-			>
-				<p className="font-medium text-slate-700">
-					Refund
-				</p>
-				<div className="mt-4">
-					<Label className="text-slate-400">Enter your password to confirm</Label>
-					<Input
-						className="w-full bg-slate-100 text-slate-700 shadow-none focus:outline focus:outline-2 focus:outline-slate-200"
-						placeholder="Password"
-					/>
-				</div>
-				<div className="mt-4 flex w-full gap-2">
-					<PopoverClose asChild>
-						<Button className="w-1/2 bg-slate-200 text-slate-700 hover:bg-slate-300">
-							Cancel
-						</Button>
-					</PopoverClose>
-					<Button className="w-1/2 bg-rose-100 text-red hover:bg-rose-200">
-						Refund
-					</Button>
-				</div>
-			</PopoverContent>
-		</Popover>
-	);
+const Refund: React.FC<RefundProps> = ({ className, onClick }) => {
+  const [password, setPassword] = useState("");
+
+  const handleRefund = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
+
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Button
+          variant="ghost"
+          className={`group flex h-12 w-12 items-center justify-center rounded-xl bg-transparent transition-all duration-300 hover:bg-rose-200/60 ${className}`}
+        >
+          <Undo2
+            className="!h-5 !w-5 text-slate-500 transition-colors duration-300 group-hover:text-rose-500"
+            strokeWidth={2.5}
+          />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="shadow-none" popoverTarget="">
+        <p className="font-medium text-slate-700">Refund</p>
+        <div className="mt-4">
+          <Label className="text-slate-400">
+            Enter your password to confirm
+          </Label>
+          <Input
+            className="w-full bg-slate-100 text-slate-700 shadow-none focus:outline focus:outline-2 focus:outline-slate-200"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="mt-4 flex w-full gap-2">
+          <PopoverClose asChild>
+            <Button className="w-1/2 bg-slate-200 text-slate-700 hover:bg-slate-300">
+              Cancel
+            </Button>
+          </PopoverClose>
+          <Button
+            className="w-1/2 bg-rose-100 text-red hover:bg-rose-200"
+            onClick={handleRefund}
+            disabled={!password}
+          >
+            Refund
+          </Button>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
 };
 
 export default Refund;
--- src/app/(protected)/admin/customers/_components/payment-record.tsx
+++ src/app/(protected)/admin/customers/_components/payment-record.tsx
@@ -1,76 +1,120 @@
-import { Calendar, User2 } from 'lucide-react';
-import { Poppins } from 'next/font/google';
-import { Separator } from '~/components/ui/separator';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '~/components/ui/tooltip';
-import Refund from '../../_components/refund';
+import { Calendar, User2 } from "lucide-react";
+import { Poppins } from "next/font/google";
+import { Separator } from "~/components/ui/separator";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
+import Refund from "../../_components/refund";
 
 const poppins = Poppins({
-	subsets: ["latin"],
-	weight: ["400", "700"],
+  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
-const PaymentRecord = () => {
-	return (
-		<div className='bg-slate-100 p-6 rounded-lg w-full'>
-			<div className="flex items-center justify-between">
-				<div className={`flex flex-col gap-1 ${poppins.className} w-full`}>
-					<p className='text-slate-600 text-lg'>#INV00000000</p>
-					<div className="flex items-center gap-4 text-slate-400">
-						<div className="flex items-center gap-2 text-slate-400">
-							<TooltipProvider delayDuration={0} skipDelayDuration={0}>
-								<Tooltip>
-									<TooltipTrigger asChild>
-										<Calendar className="h-4 w-4" />
-									</TooltipTrigger>
-									<TooltipContent className='text-slate-700 bg-white rounded-lg text-sm shadow-none border border-slate-200'>
-										Payment recorded on February 05, 2025
-									</TooltipContent>
-								</Tooltip>
-							</TooltipProvider>
-							<p className="text-sm">February 05, 2025</p>
-						</div>
-						<Separator
-							orientation="vertical"
-							className="h-5 w-[2px] bg-slate-200"
-						/>
-						<div className="flex items-center gap-2 text-slate-400">
-							<TooltipProvider delayDuration={0} skipDelayDuration={0}>
-								<Tooltip>
-									<TooltipTrigger asChild>
-										<User2 className="h-4 w-4" />
-									</TooltipTrigger>
-									<TooltipContent className='text-slate-700 bg-white rounded-lg text-sm shadow-none border border-slate-200'>
-										Recorded by Bob Admin
-									</TooltipContent>
-								</Tooltip>
-							</TooltipProvider>
-							<p className="text-sm">Bob Admin</p>
-						</div>
-					</div>
-				</div>
-				<Separator
-					orientation="vertical"
-					className="h-12 w-[2px] bg-slate-200"
-				/>
-				<div className="flex items-center justify-between ${poppins.className} w-full ml-6">
-					<div className={`flex flex-col gap-1`}>
-						<p className='text-slate-600 text-lg'>₱5000</p>
-						<div className="flex items-center gap-4 text-slate-400">
-							<p className="text-sm">Amount paid</p>
-							<Separator
-								orientation="vertical"
-								className="h-5 w-[2px] bg-slate-200"
-							/>
-							<div className="flex items-center gap-3">
-								<p className="text-sm">0 Change</p>
-							</div>
-						</div>
-					</div>
-					<Refund />
-				</div>
-			</div>
-		</div>
-	)
+interface PaymentRecordProps {
+  payment: {
+    payment_id: number;
+    invoice_id: number;
+    amount: number;
+    payment_date: Date;
+    payment_method: string;
+    reference?: string | null;
+    invoice?: {
+      invoice_number: number;
+    };
+    PaymentLog?: {
+      status: string;
+      timestamp: Date;
+    }[];
+  };
+  onRefund?: () => void;
 }
 
-export default PaymentRecord
+const PaymentRecord = ({ payment, onRefund }: PaymentRecordProps) => {
+  const formattedDate = new Date(payment.payment_date).toLocaleDateString(
+    "en-US",
+    {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    },
+  );
+
+  // Format the invoice number with leading zeros
+  const formattedInvoiceNumber =
+    payment.invoice?.invoice_number.toString().padStart(8, "0") || "00000000";
+
+  // Format the amount with 2 decimal places
+  const formattedAmount = payment.amount.toFixed(2);
+
+  return (
+    <div className="w-full rounded-lg bg-slate-100 p-6">
+      <div className="flex items-center justify-between">
+        <div className={`flex flex-col gap-1 ${poppins.className} w-full`}>
+          <p className="text-lg text-slate-600">#{formattedInvoiceNumber}</p>
+          <div className="flex items-center gap-4 text-slate-400">
+            <div className="flex items-center gap-2 text-slate-400">
+              <TooltipProvider delayDuration={0} skipDelayDuration={0}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Calendar className="h-4 w-4" />
+                  </TooltipTrigger>
+                  <TooltipContent className="rounded-lg border border-slate-200 bg-white text-sm text-slate-700 shadow-none">
+                    Payment recorded on {formattedDate}
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+              <p className="text-sm">{formattedDate}</p>
+            </div>
+            <Separator
+              orientation="vertical"
+              className="h-5 w-[2px] bg-slate-200"
+            />
+            <div className="flex items-center gap-2 text-slate-400">
+              <TooltipProvider delayDuration={0} skipDelayDuration={0}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <User2 className="h-4 w-4" />
+                  </TooltipTrigger>
+                  <TooltipContent className="rounded-lg border border-slate-200 bg-white text-sm text-slate-700 shadow-none">
+                    Payment method: {payment.payment_method}
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+              <p className="text-sm">{payment.payment_method}</p>
+            </div>
+          </div>
+        </div>
+        <Separator
+          orientation="vertical"
+          className="h-12 w-[2px] bg-slate-200"
+        />
+        <div className="${poppins.className} ml-6 flex w-full items-center justify-between">
+          <div className={`flex flex-col gap-1`}>
+            <p className="text-lg text-slate-600">₱{formattedAmount}</p>
+            <div className="flex items-center gap-4 text-slate-400">
+              <p className="text-sm">Amount paid</p>
+              <Separator
+                orientation="vertical"
+                className="h-5 w-[2px] bg-slate-200"
+              />
+              <div className="flex items-center gap-3">
+                <p className="text-sm">
+                  {payment.reference
+                    ? `Ref: ${payment.reference}`
+                    : "No reference"}
+                </p>
+              </div>
+            </div>
+          </div>
+          <Refund onClick={onRefund} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentRecord;
